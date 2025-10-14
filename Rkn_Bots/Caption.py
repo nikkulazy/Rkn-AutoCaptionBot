b3e7ed8441328dcc1f4d49ff2cd9970d72b16bb7
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2025-10-15 00:18:37 +0530

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -68,10 +68,10 @@ async def start_cmd(bot, message):
     await message.reply_photo(photo=Rkn_Bots.RKN_PIC,
         caption=f"<b>Hey, {message.from_user.mention}\n\nI'm an auto-caption bot. I automatically edit captions for videos, audio files, and documents posted on channels.\n\nuse <code>/set_caption</code> to set caption\nUse<code>/delcaption</code> To delete caption and set caption to default.\n\nNote:All commands works on channels only</b>",
         reply_markup=types.InlineKeyboardMarkup([[
-            types.InlineKeyboardButton('Main Channel', url='https://t.me/Techshyam007'),
-            types.InlineKeyboardButton('Help Group', url='https://t.me/techshyaam')
+            types.InlineKeyboardButton('Main Channel', url='https://t.me/wolverine273'),
+            types.InlineKeyboardButton('Help Group', url='https://t.me/WOLVERIN_P')
             ],[
-            types.InlineKeyboardButton('🔥 Source Code 🔥', url='https://t.me/WOLVERIN_P')
+            types.InlineKeyboardButton('♻️ADD ME YOUR CHANNEL♻️', url=f'http://telegram.me/{temp.U_NAME}?startchannel=true')
     ]]))
     
 
