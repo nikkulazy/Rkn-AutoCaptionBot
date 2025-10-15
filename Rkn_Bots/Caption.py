30bc555c40134f62f7edda9fc0dedfcb42649e64
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2025-10-15 11:49:51 +0530

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -71,7 +71,7 @@ async def start_cmd(bot, message):
             types.InlineKeyboardButton('Main Channel', url='https://t.me/wolverine273'),
             types.InlineKeyboardButton('Help Group', url='https://t.me/WOLVERIN_P')
             ],[
-            types.InlineKeyboardButton('♻️ADD ME YOUR CHANNEL♻️', url=f'https://t.me/thinkfilmy')
+            types.InlineKeyboardButton('♻️ADD ME YOUR CHANNEL♻️', url='https://t me/thinkfilmy')
     ]]))
     
 
