baa2381eb9b67a1d9fe1631ed6d958054b38eda3
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-07-18 09:57:42 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -1,3 +1,9 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+
 import motor.motor_asyncio
 from config import Rkn_Bots
 
