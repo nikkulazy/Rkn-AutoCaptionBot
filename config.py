e21161a2677c37107ff588f0e0a192bfae2afad0
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-08-28 00:24:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,21 +29,16 @@ class Rkn_Bots(object):
     DB_URL = os.environ.get("DB_URL", "")
 
     # default caption 
-    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='telegram.me/WOLVERIN_P'>{file_name}</a></b>",
-    )
+    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='telegram.me/WOLVERIN_P'>{file_name}</a></b>")
 
     # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
     # admin id  ( required.. 😥)
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5346278816').split()]
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001521000125")  #
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001521000125")
 
 # Rkn Developer 
 # Don't Remove Credit 😔
 # Telegram Channel @RknDeveloper & @Rkn_Botz
 # Developer @RknDeveloperr
-
-
-
-
